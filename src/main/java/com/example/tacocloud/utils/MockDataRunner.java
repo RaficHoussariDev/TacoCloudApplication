b7decffded84e88890a735f3ad9bebfc1c977604
Rafic Houssari <rafic.houssari@cedrusco.com>
2022-09-26 16:08:42 +0300

    Migrate from JDBC template data persistence to JPA

---
+++ src/main/java/com/example/tacocloud/utils/MockDataRunner.java
@@ -0,0 +1,32 @@
+package com.example.tacocloud.utils;
+
+import com.example.tacocloud.models.Ingredient;
+import com.example.tacocloud.models.Ingredient.Type;
+import com.example.tacocloud.repositories.ingredient.IngredientRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class MockDataRunner implements CommandLineRunner {
+    @Autowired
+    private IngredientRepository repo;
+
+    @Override
+    public void run(String... args) throws Exception {
+        repo.save(new Ingredient("FLTO", "Flour Tortilla", Type.WRAP));
+        repo.save(new Ingredient("COTO", "Corn Tortilla", Type.WRAP));
+        repo.save(new Ingredient("GRBF", "Ground Beef", Type.PROTEIN));
+        repo.save(new Ingredient("CARN", "Carnitas", Type.PROTEIN));
+        repo.save(new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES));
+        repo.save(new Ingredient("LETC", "Lettuce", Type.VEGGIES));
+        repo.save(new Ingredient("CHED", "Cheddar", Type.CHEESE));
+        repo.save(new Ingredient("JACK", "Monterrey Jack", Type.CHEESE));
+        repo.save(new Ingredient("SLSA", "Salsa", Type.SAUCE));
+        repo.save(new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
+    }
+}
